a57fb2dd61c8ad9c999b28ce22116b95ad69cfcd
poorna30646 <thotipoornachandra@gmail.com>
2024-12-14 14:57:39 +0530

    Add files via upload

---
+++ exception2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class fire{
+    public static void main(String args[]) throws ArithmeticException{
+        int a=10;
+        int b=0;
+        System.out.println(a/b);
+    }
+
+    }
